105c455acb92ceeb59c268d16fe414eb9d2a5e02
xiaoyang <xiao1.developer@gmail.com>
2025-03-02 18:48:05 +0100

    Refactor: Add Internationalization and Localization with GetX
    - Integrates GetX for internationalization and localization.
    - Adds support for English, Spanish, and Chinese.
    - Defines translation keys in `app_messages.dart`.
    - Creates translation files for English (`en.dart`), Spanish (`es.dart`), and Chinese (`zh.dart`).
    - Configures `GetMaterialApp` to use `AppTranslations` for translations.
    - Displays localized text using `tr` extension.
    - Adds buttons to switch between languages.
    - Removes unnecessary template code.
    - Adds `provider` dependency.
    - Updates `pubspec.yaml` file.
    - Adds `flutter_localizations` dependency.
    - Updates `README.md` file.

--- lib/app/config/translation/app_translations.dart
+++ lib/app/config/translation/app_translations.dart
@@ -1,18 +1,27 @@
 import 'dart:ui';
-
 import 'package:get/get.dart';
 
 part 'app_messages.dart';
+
 part 'en.dart';
-part 'id.dart';
+
+part 'es.dart';
+
+part 'zh.dart';
 
 class AppTranslations extends Translations {
   static Locale? get locale => Get.deviceLocale;
+
   static Locale? get fallbackLocale => const Locale('en');
+
+  static const Locale ENGLISH = Locale('en');
+  static const Locale SPANISH = Locale('es');
+  static const Locale CHINIES = Locale('zh');
 
   @override
   Map<String, Map<String, String>> get keys => {
         'en': en.messages,
-        'id': id.messages,
+        'es': es.messages,
+        'zh': zh.messages,
       };
 }
---
+++ lib/app/config/translation/zh.dart
@@ -0,0 +1,13 @@
+
+part of 'app_translations.dart';
+
+
+class ChineseMessages extends BaseMessages {
+  @override
+  String get accountType => "帐户类型";
+
+  @override
+  String get alreadyHaveAccount => "已经有帐户了吗？";
+}
+
+final zh = ChineseMessages();
