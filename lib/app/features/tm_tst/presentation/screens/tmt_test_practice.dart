91c913e9689ff13f483d6594b0929c12cfbb8afb
xiaoyang <xiao1.developer@gmail.com>
2025-03-16 22:20:17 +0100

    Feat: Add TMT Test Practice Screen and Navigation
    - Adds a new screen for TMT Test practice (`TmtTestPracticePage`).
    - Creates a new route (`tmt_practice`) for the practice screen.
    - Implements navigation from the help screen to the practice screen.
    - Adds a `NavigationMixin` function `tmmHelpToPractice` to navigate to the practice screen.
    - Implements back navigation from the practice screen to the main test.
    - Updates the TMT Test help screen to include a button that navigates to the practice screen.
    - Updates `app_pages` to include the `tmt_practice` route.
    - Updates `app_routes` to include the path `tmt_practice`.
    - Updates `navigation_mixin` to add `tmmHelpToPractice` function.

--- lib/app/features/tm_tst/presentation/screens/tmt_test_practice.dart
+++ lib/app/features/tm_tst/presentation/screens/tmt_test_practice.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../../config/translation/app_translations.dart';
 import '../../../../shared_components/custom_app_bar.dart';
 import '../../../../shared_components/custom_primary_button.dart';
 import '../../../../shared_components/custom_secondary_button.dart';
 import '../../../../utils/helpers/app_helpers.dart';
-import '../../../../utils/mixins/app_mixins.dart';
-import '../controllers/tmt_test_flow_state_controller.dart';
 
-class TmtTestHelpPage extends StatelessWidget with NavigationMixin {
-  const TmtTestHelpPage({super.key});
+class TmtTestPracticePage extends StatelessWidget {
+  const TmtTestPracticePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    TmtTestStateFlow tmtTestStateFlow;
-    try {
-      tmtTestStateFlow = Get.arguments as TmtTestStateFlow;
-    } catch (e) {
-      tmtTestStateFlow = TmtTestStateFlow.TMT_A_IN_PROGRESS;
-    }
     return Scaffold(
       appBar: CustomAppBar(
-        title: _getHelpTitle(tmtTestStateFlow),
+        title: "Practicar",
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
@@ -32,7 +23,7 @@ class TmtTestHelpPage extends StatelessWidget with NavigationMixin {
             Expanded(
               child: Center(
                 child: Text(
-                  "Aquí se mostrará\nun video,\nanimación o texto\npara describir\nlas instrucciones\npara realizar\nel test TMT B",
+                  "Practicar",
                   textAlign: TextAlign.center,
                   style: const TextStyle(
                     fontSize: 24,
@@ -47,17 +38,15 @@ class TmtTestHelpPage extends StatelessWidget with NavigationMixin {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   CustomPrimaryButton(
-                    text: TMTGame.tmtGameTmtHelpTmtPrimaryButtonText.tr,
+                    text: "Terminar",
                     onPressed: () {
-                      tmmHelpToPractice();
+                      Get.close(2);
                     },
                   ),
                   SizedBox(height: DeviceHelper.isTablet ? 28 : 22),
                   CustomSecondaryButton(
-                    text: TMTGame.tmtGameTmtHelpTmtSecondaryButtonText.tr,
-                    onPressed: () {
-                      Get.back();
-                    },
+                    text: "Repetir",
+                    onPressed: () {},
                   ),
                 ],
               ),
@@ -66,16 +55,5 @@ class TmtTestHelpPage extends StatelessWidget with NavigationMixin {
         ),
       ),
     );
-  }
-
-  String _getHelpTitle(TmtTestStateFlow tmtTestStateFlow) {
-    switch (tmtTestStateFlow) {
-      case TmtTestStateFlow.TMT_A_IN_PROGRESS:
-        return TMTGame.tmtGameTmtHelpTmtATitle.tr;
-      case TmtTestStateFlow.TMT_B_IN_PROGRESS:
-        return TMTGame.tmtGameTmtHelpTmtBTitle.tr;
-      default:
-        return TMTGame.tmtGameTmtHelpTmtATitle.tr;
-    }
   }
 }
