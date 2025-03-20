832f3a3327a898d6ab64362cb4e42d0d9ce6d686
xiaoyang <xiao1.developer@gmail.com>
2025-03-21 00:43:04 +0100

    Refactor: Update Text Styles and Add Font Weights
    - Replaces `FontWeight` enums with `AppFontWeight` in `AppTextStyle`.
    - Updates font sizes based on device type (tablet/mobile) for custom dialog content and buttons.
    - Adjusts font weight for dialog components for better readability.

--- lib/app/config/themes/AppTextStyle.dart
+++ lib/app/config/themes/AppTextStyle.dart
@@ -1,55 +1,54 @@
 import 'package:flutter/material.dart';
 import '../../utils/helpers/app_helpers.dart';
 import 'AppColors.dart';
+import 'AppFontWeight.dart';
 
 class AppTextStyle {
-
   static var appBarTitle = TextStyle(
     color: AppColors.mainBlackText,
     fontSize: DeviceHelper.isTablet ? 16.0 : 14.0,
-    fontWeight: FontWeight.bold,
+    fontWeight: AppFontWeight.bold,
     fontStyle: FontStyle.normal,
   );
 
   static var tmtGameCircleText = TextStyle(
     color: AppColors.mainBlackText,
     fontSize: DeviceHelper.isTablet ? 30 : 22,
-    fontWeight: FontWeight.bold,
+    fontWeight: AppFontWeight.bold,
     fontStyle: FontStyle.normal,
   );
 
   static var tmtGameCircleBeginAndLastText = TextStyle(
     color: AppColors.mainBlackText,
     fontSize: DeviceHelper.isTablet ? 28 : 18,
-    fontWeight: FontWeight.bold,
+    fontWeight: AppFontWeight.bold,
     fontStyle: FontStyle.normal,
   );
 
   static var customDialogTitle = TextStyle(
     color: AppColors.customDialogTitleColor,
     fontSize: 16.0,
-    fontWeight: FontWeight.bold,
+    fontWeight: AppFontWeight.extraBold,
     fontStyle: FontStyle.normal,
   );
 
   static var customDialogContent = TextStyle(
     color: Color(0xFF71727A),
-    fontSize: 12.0,
-    fontWeight: FontWeight.normal,
+    fontSize: DeviceHelper.isTablet ? 14 : 12,
+    fontWeight: AppFontWeight.normal,
   );
 
   static var customDialogButton = TextStyle(
     color: Colors.white,
-    fontSize: 14.0,
-    fontWeight: FontWeight.normal,
+    fontSize: DeviceHelper.isTablet ? 14 : 12,
+    fontWeight: AppFontWeight.semiBold,
     fontStyle: FontStyle.normal,
   );
 
   static var customDialogOutlinedButton = TextStyle(
     color: AppColors.customButtonColor,
-    fontSize: 14.0,
-    fontWeight: FontWeight.normal,
+    fontSize: DeviceHelper.isTablet ? 14 : 12,
+    fontWeight: AppFontWeight.semiBold,
     fontStyle: FontStyle.normal,
   );
-
 }
