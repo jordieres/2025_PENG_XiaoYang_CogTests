0c2a3601ea00196819dde68c2f154ed68f783acc
xiaoyang <xiao1.developer@gmail.com>
2025-03-23 19:17:44 +0100

    Refactor: Restructure Network Services and Enhance API Communication
    - Removes the old `RestApiServices` and create new one in `net` folder.
    - Adds `ResultData` class for handling API responses.
    - Introduces `ApiConstants` for managing API-related constants.
    - Implement `validateReferenceCode`, `reportTmtResults`, and `listTestResults` in the new RestApiServices.
    - Configures Dio with request/response logging.
    - Updates `main.dart` to initialize `AppLogger` and `DeviceHelper`.
    - Adds `equalsIgnoreCase` method to `StringHelper`.
    - Adds `shared_preferences`, `dio`, `logging` in `pubspec.yaml` dependencies.
    - Creates `api_path.dart` with endpoits url.

---
+++ lib/app/utils/services/net/api_constants.dart
@@ -0,0 +1,45 @@
+part of 'rest_api_services.dart';
+
+class ApiConstants {
+  static const String validateReferenceCodeTag = "validateReferenceCode";
+  static const String reportTmtResultsTag = "reportTmtResults";
+  static const String listTestResultsTag = "listTestResults";
+  static const String dateFormatSeparator = "-";
+  static const String padding = "0";
+  static const int padWidth = 2;
+
+  static const String messageField = "message";
+  static const String statusField = "status";
+  static const String dataField = "data";
+  static const String statusOk = "OK";
+  static const String codeIdField = "codeid";
+  static const String dateDataField = "date_data";
+
+  static const String birthDateField = "F_nacimiento";
+  static const String sexField = "Sexo";
+  static const String educLevelField = "Nivel_Educ";
+  static const String handField = "Mano";
+  static const String numCircField = "NumCirc";
+  static const String timeCompleteField = "Time_complete";
+  static const String numberErrorsField = "Number_Errors";
+  static const String averagePauseField = "Average_Pause";
+  static const String averageLiftField = "Average_Lift";
+  static const String averageRateBetweenCirclesField = "Average_Rate_Between_Circles";
+  static const String averageRateInsideCirclesField = "Average_Rate_Inside_Circles";
+  static const String averageTimeBetweenCirclesField = "Average_Time_Between_Circles";
+  static const String averageTimeInsideCirclesField = "Average_Time_Inside_Circles";
+  static const String averageRateBeforeLettersField = "Average_Rate_Before_Letters";
+  static const String averageRateBeforeNumbersField = "Average_Rate_Before_Numbers";
+  static const String averageTimeBeforeLettersField = "Average_Time_Before_Letters";
+  static const String averageTimeBeforeNumbersField = "Average_Time_Before_Numbers";
+  static const String numberPausesField = "Number_Pauses";
+  static const String numberLiftsField = "Number_Lifts";
+  static const String averageTotalPressureField = "Average_Total_Pressure";
+  static const String averageTotalSizeField = "Average_Total_Size";
+  static const String scoreField = "Score";
+  static const String numberErrorAField = "Number_Error_A";
+  static const String numberErrorBField = "Number_Error_B";
+  static const String timeCompleteAField = "Time_Complete_A";
+  static const String timeCompleteBField = "Time_Complete_B";
+  static const String deviceModelField = "Device_Model";
+}
