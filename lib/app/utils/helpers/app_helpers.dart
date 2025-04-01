e82718456927b85f89dc2371216e078d9833d0b2
xiaoyang <xiao1.developer@gmail.com>
2025-04-01 23:53:04 +0200

    feat: Added score for 5 sections

--- lib/app/utils/helpers/app_helpers.dart
+++ lib/app/utils/helpers/app_helpers.dart
@@ -1,4 +1,5 @@
 library app_helpers;
+
 import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
@@ -6,5 +7,9 @@ import 'package:flutter/cupertino.dart';
 import '../services/app_logger.dart';
 
 part 'string_helper.dart';
+
+part 'double_helper.dart';
+
 part 'type.dart';
+
 part 'device_helper.dart';
