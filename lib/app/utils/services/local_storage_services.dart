e8c36a9f74d15b76f3fcee0548b40f3aaa7f9be1
xiaoyang <xiao1.developer@gmail.com>
2025-04-04 00:32:34 +0200

    Refactor: Consolidate theme settings persistence into LocalStorageServices
    Theme settings persistence has been refactored from `ThemeController` into a centralized `LocalStorageServices`. This change enhances code organization and maintainability by grouping all local storage operations within a dedicated service.

--- lib/app/utils/services/local_storage_services.dart
+++ lib/app/utils/services/local_storage_services.dart
@@ -1,15 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-/// contains all service to get data from local
 class LocalStorageServices {
   static final LocalStorageServices _localStorageServices =
-      LocalStorageServices._internal();
+  LocalStorageServices._internal();
 
   factory LocalStorageServices() {
     return _localStorageServices;
   }
 
   static const String _pendingResultsKey = 'pending_tmt_results';
+  static const String _themeKey = 'theme_mode';
+  static const String _systemThemeKey = 'system_theme';
 
   LocalStorageServices._internal();
 
@@ -26,5 +28,38 @@ class LocalStorageServices {
   static Future<bool> clearPendingResultList() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return await prefs.remove(_pendingResultsKey);
+  }
+
+  static Future<bool> saveThemeSettings(bool isFollowSystem, ThemeMode themeMode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_systemThemeKey, isFollowSystem);
+
+    if (!isFollowSystem) {
+      await prefs.setString(
+          _themeKey,
+          themeMode == ThemeMode.dark
+              ? ThemeMode.dark.name
+              : ThemeMode.light.name);
+    }
+    return true;
+  }
+
+  static Future<Map<String, dynamic>> loadThemeSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isSystemTheme = prefs.getBool(_systemThemeKey) ?? true;
+
+    ThemeMode themeMode = ThemeMode.system;
+    if (!isSystemTheme) {
+      final themeValue = prefs.getString(_themeKey);
+      if (themeValue != null) {
+        themeMode = themeValue == ThemeMode.dark.name
+            ? ThemeMode.dark
+            : ThemeMode.light;
+      }
+    }
+    return {
+      'isFollowSystem': isSystemTheme,
+      'themeMode': themeMode
+    };
   }
 }
