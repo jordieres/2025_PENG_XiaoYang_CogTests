6a327dfc163ce921609a223d3a7a0f1084085176
xiaoyang <xiao1.developer@gmail.com>
2025-04-08 20:57:15 +0200

    Refactor: Implement user profile registration with form validation and UI improvements
    This commit refactors the user profile registration screen, introducing form validation and enhancing the user interface.
    
    Specific changes include:
    
    - Added form validation for nickname, sex, birth date, and education level fields.
    - Improved UI with better layout, input decoration, and styling.
    - Implemented logic to prevent multiple snackbars from displaying simultaneously.
    - Integrated localization for date picker and other text elements.
    - Added a unique user ID generation using the `uuid` package.
    - Updated button styling and added an `isEnabled` property to control button state.
    - Modified the user profile entity and model to handle user ID internally.
    - Added new translations for form-related texts in Spanish and Chinese.
    - Introduced a custom input decoration class for consistent form field styling.
    - Updated the main application file to include localization delegates and supported locales.

--- lib/app/utils/ui/app_snackbar.dart
+++ lib/app/utils/ui/app_snackbar.dart
@@ -2,34 +2,46 @@ part of ui_utils;
 
 /// contains all snackbar templates
 class AppSnackbar {
+  static bool _isSnackbarVisible = false;
+
   static void showCustomSnackbar(
     BuildContext context,
     String message, {
     int durationInSeconds = 2,
     Color backgroundColor = Colors.black87,
   }) {
+    if (_isSnackbarVisible) {
+      return;
+    }
+
+    _isSnackbarVisible = true;
     final screenWidth = MediaQuery.of(context).size.width;
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Center(
-          child: Text(
-            message,
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white),
+    ScaffoldMessenger.of(context)
+        .showSnackBar(
+          SnackBar(
+            content: Center(
+              child: Text(
+                message,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            duration: Duration(seconds: durationInSeconds),
+            behavior: SnackBarBehavior.floating,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: EdgeInsets.symmetric(
+              horizontal: screenWidth * 0.2, // 20% padding on both sides
+              vertical: 12,
+            ),
+            backgroundColor: backgroundColor,
           ),
-        ),
-        duration: Duration(seconds: durationInSeconds),
-        behavior: SnackBarBehavior.floating,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        padding: EdgeInsets.symmetric(
-          horizontal: screenWidth * 0.2, // 20% padding on both sides
-          vertical: 12,
-        ),
-        backgroundColor: backgroundColor,
-      ),
-    );
+        )
+        .closed
+        .then((_) {
+      _isSnackbarVisible = false;
+    });
   }
 }
