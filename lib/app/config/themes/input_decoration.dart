8ed45bd8f6823d8cf4005863beeb70ae044604d5
xiaoyang <xiao1.developer@gmail.com>
2025-04-08 22:02:22 +0200

    feat: Register User Screen Improvements
    - Enhance Register User Screen with improved form field validation and focus management.
    - Add focus nodes for nickname, birth date, and education level fields to trigger validation on focus change.
    - Implement listeners to handle focus changes and validate fields accordingly.
    - Update UI for better user experience, including date picker integration with text field and styled input fields.
    - Show snackbar messages for form validation errors and successful registration.

--- lib/app/config/themes/input_decoration.dart
+++ lib/app/config/themes/input_decoration.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 import 'AppColors.dart';
 import 'app_text_style_base.dart';
@@ -6,32 +8,38 @@ import 'app_text_style_base.dart';
 class CustomInputDecoration {
   static final BorderRadius _defaultBorderRadius = BorderRadius.circular(12);
 
-  static InputDecoration commonInputDecoration = InputDecoration(
-    border: OutlineInputBorder(
-      borderRadius: _defaultBorderRadius,
-    ),
-    enabledBorder: OutlineInputBorder(
-      borderRadius: _defaultBorderRadius,
-      borderSide: BorderSide(color: Colors.grey.shade400),
-    ),
-    focusedBorder: OutlineInputBorder(
-      borderRadius: _defaultBorderRadius,
-      borderSide: BorderSide(color: AppColors.primaryBlue, width: 2),
-    ),
-    errorBorder: OutlineInputBorder(
-      borderRadius: _defaultBorderRadius,
-      borderSide: BorderSide(color: AppColors.mainRed, width: 2),
-    ),
-    focusedErrorBorder: OutlineInputBorder(
-      borderRadius: _defaultBorderRadius,
-      borderSide: BorderSide(color: AppColors.mainRed, width: 2),
-    ),
-    hintStyle: TextStyleBase.bodyM.copyWith(
-      color: Color(0xFF8F9098),
-    ),
-    contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 18),
-  );
+  static final _BORDER_WIDTH = 2.0;
 
-  static var textInputStyle = TextStyleBase.bodyM.copyWith(
-  );
+  static final focusColor =
+      Get.isDarkMode ? AppColors.primaryBlueDark : AppColors.primaryBlue;
+
+  static InputDecoration commonInputDecoration() {
+    return InputDecoration(
+      border: OutlineInputBorder(
+        borderRadius: _defaultBorderRadius,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: _defaultBorderRadius,
+        borderSide: BorderSide(color: Colors.grey.shade400),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: _defaultBorderRadius,
+        borderSide: BorderSide(color: focusColor, width: _BORDER_WIDTH),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: _defaultBorderRadius,
+        borderSide: BorderSide(color: AppColors.mainRed, width: _BORDER_WIDTH),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: _defaultBorderRadius,
+        borderSide: BorderSide(color: AppColors.mainRed, width: _BORDER_WIDTH),
+      ),
+      hintStyle: TextStyleBase.bodyM.copyWith(
+        color: Color(0xFF8F9098),
+      ),
+      contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+    );
+  }
+
+  static var textInputStyle = TextStyleBase.bodyM.copyWith();
 }
--- lib/app/features/user/presentation/screen/register_user_screen.dart
+++ lib/app/features/user/presentation/screen/register_user_screen.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 
 import '../../../../config/themes/AppColors.dart';
 import '../../../../config/themes/input_decoration.dart';
+import '../../../../utils/ui/ui_utils.dart';
 import '../../domain/entities/user_profile.dart';
 import '../contoller/user_profile_controller.dart';
 import '../../../../shared_components/custom_app_bar.dart';
@@ -21,6 +22,10 @@ class RegisterUserScreen extends StatefulWidget {
 class _RegisterUserScreenState extends State<RegisterUserScreen> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController nicknameController = TextEditingController();
+  final TextEditingController birthDateController = TextEditingController();
+  final FocusNode nicknameFocusNode = FocusNode();
+  final FocusNode birthDateFocusNode = FocusNode();
+  final FocusNode educationLevelFocusNode = FocusNode();
 
   Sex? selectedSex;
   DateTime? selectedBirthDate;
@@ -45,21 +50,43 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
   void initState() {
     super.initState();
     controller = Get.find<UserProfileController>();
+    nicknameFocusNode.addListener(_onNicknameFocusChange);
+    birthDateFocusNode.addListener(_onBirthDateFocusChange);
+    educationLevelFocusNode.addListener(_onEducationLevelFocusChange);
+  }
+
+  void _onNicknameFocusChange() {
+    if (nicknameFocusNode.hasFocus) {
+      _formKey.currentState?.validate();
+    }
+  }
+
+  void _onBirthDateFocusChange() {
+    if (birthDateFocusNode.hasFocus) {
+      _formKey.currentState?.validate();
+    }
+  }
+
+  void _onEducationLevelFocusChange() {
+    if (educationLevelFocusNode.hasFocus) {
+      _formKey.currentState?.validate();
+    }
   }
 
   @override
   void dispose() {
     nicknameController.dispose();
+    birthDateController.dispose();
+    nicknameFocusNode.removeListener(_onNicknameFocusChange);
+    birthDateFocusNode.removeListener(_onBirthDateFocusChange);
+    educationLevelFocusNode.removeListener(_onEducationLevelFocusChange);
+    nicknameFocusNode.dispose();
+    birthDateFocusNode.dispose();
+    educationLevelFocusNode.dispose();
     super.dispose();
   }
 
   Future<void> _selectDate(BuildContext context) async {
-    setState(() {
-      _birthDateVisited = true;
-      _nicknameVisited = true;
-      _sexVisited = true;
-    });
-
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: selectedBirthDate ?? DateTime.now(),
@@ -73,6 +100,19 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
     if (picked != null && picked != selectedBirthDate) {
       setState(() {
         selectedBirthDate = picked;
+        _birthDateVisited = true;
+        _nicknameVisited = true;
+        _sexVisited = true;
+        final currentLocale = Get.locale?.toString() ?? 'en_US';
+        birthDateFocusNode.removeListener(_onBirthDateFocusChange);
+        birthDateController.text = DateFormat.yMd(currentLocale).format(picked);
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          birthDateController.selection = TextSelection.collapsed(
+            offset: birthDateController.text.length,
+          );
+        });
+
+        birthDateFocusNode.addListener(_onBirthDateFocusChange);
       });
     }
 
@@ -107,13 +147,10 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
         selectedSex == null ||
         selectedBirthDate == null ||
         selectedEducationLevel == null) {
-      Get.snackbar(
-        'Formulario incompleto',
+      AppSnackbar.showCustomSnackbar(
+        context,
         'Por favor, rellena todos los campos marcados.',
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: Colors.red.withOpacity(0.8),
-        colorText: Colors.white,
-        margin: EdgeInsets.all(8),
+        backgroundColor: AppColors.mainRed.withAlpha(204),
       );
       return;
     }
@@ -131,19 +168,16 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
         birthDate: selectedBirthDate!,
         educationLevel: selectedEducationLevel!,
       );
-
       controller.saveProfile(newUserProfile);
-
       Get.back();
+      AppSnackbar.showCustomSnackbar(
+          context, 'Usuario registrado correctamente');
     } catch (e) {
       if (!mounted) return;
-      Get.snackbar(
-        'Error',
-        'Error al guardar el perfil: $e',
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: Colors.red.withOpacity(0.8),
-        colorText: Colors.white,
-        margin: EdgeInsets.all(8),
+      AppSnackbar.showCustomSnackbar(
+        context,
+        'Por favor, rellena todos los campos marcados.',
+        backgroundColor: AppColors.mainRed.withAlpha(204),
       );
     } finally {
       if (mounted) {
@@ -156,17 +190,13 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final currentLocale = Get.locale?.toString() ?? 'en_US';
     final mediaQuery = MediaQuery.of(context);
     final isLandscape = mediaQuery.orientation == Orientation.landscape;
-    final isDarkMode = Get.isDarkMode;
     final appBar = CustomAppBar(title: 'Mis Datos');
     final availableHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
         mediaQuery.padding.top -
         mediaQuery.padding.bottom;
-
-    final GlobalKey formKey = GlobalKey();
 
     return Scaffold(
       appBar: appBar,
@@ -183,14 +213,13 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      key: formKey,
-                      child: _buildFormFields(
-                          currentLocale, isLandscape, isDarkMode),
+                      key: _formKey,
+                      child: _buildFormFields(isLandscape),
                     ),
                     FutureBuilder(
                       future: Future.delayed(Duration(milliseconds: 50), () {
-                        if (formKey.currentContext != null) {
-                          final RenderBox box = formKey.currentContext!
+                        if (_formKey.currentContext != null) {
+                          final RenderBox box = _formKey.currentContext!
                               .findRenderObject() as RenderBox;
                           final formHeight = box.size.height;
                           final remainingSpace =
@@ -215,166 +244,200 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
     );
   }
 
-  Widget _buildFormFields(
-      String currentLocale, bool isLandscape, bool isDarkMode) {
-    final primaryColor =
-        isDarkMode ? AppColors.primaryBlueDark : AppColors.primaryBlue;
-
+  Widget _buildFormFields(bool isLandscape) {
     return Form(
       key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Apodo', style: _labelStyle),
-          const SizedBox(height: 8),
-          TextFormField(
-            controller: nicknameController,
-            decoration: CustomInputDecoration.commonInputDecoration.copyWith(
-              hintText: 'Introduce un apodo',
-            ),
-            validator: (value) {
-              if (_nicknameVisited || _formSubmitted) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor introduce un apodo';
-                }
-              }
-              return null;
-            },
-            onTap: () => setState(() => _nicknameVisited = true),
-            onChanged: (value) {
-              setState(() => _nicknameVisited = true);
-            },
-            style: CustomInputDecoration.textInputStyle,
-          ),
+          _buildNicknameField(),
           const SizedBox(height: 24),
-          Text('Sexo', style: _labelStyle),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 150,
-                child: RadioListTile<Sex>(
-                  title: const Text('Masculino'),
-                  value: Sex.male,
-                  groupValue: selectedSex,
-                  activeColor: primaryColor,
-                  onChanged: (Sex? value) {
-                    setState(() {
-                      selectedSex = value;
-                      _sexVisited = true;
-                      _nicknameVisited = true;
-                    });
-                    _formKey.currentState?.validate();
-                  },
-                  contentPadding: EdgeInsets.zero,
-                  visualDensity: VisualDensity.compact,
-                ),
-              ),
-              SizedBox(width: 20),
-              SizedBox(
-                width: 150,
-                child: RadioListTile<Sex>(
-                  title: const Text('Femenino'),
-                  value: Sex.female,
-                  groupValue: selectedSex,
-                  activeColor: primaryColor,
-                  onChanged: (Sex? value) {
-                    setState(() {
-                      selectedSex = value;
-                      _sexVisited = true;
-                      _nicknameVisited = true;
-                    });
-                    _formKey.currentState?.validate();
-                  },
-                  contentPadding: EdgeInsets.zero,
-                  visualDensity: VisualDensity.compact,
-                ),
-              ),
-            ],
-          ),
-          if ((_sexVisited || _formSubmitted) && selectedSex == null)
-            Padding(
-              padding: const EdgeInsets.only(top: 0, left: 12.0, bottom: 8.0),
-              child: Text(
-                'Por favor selecciona un sexo',
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.error, fontSize: 12),
-              ),
-            ),
+          _buildSexField(),
           const SizedBox(height: 16),
-          Text('Fecha de nacimiento', style: _labelStyle),
-          const SizedBox(height: 8),
-          InkWell(
-            onTap: () => _selectDate(context),
-            child: InputDecorator(
-                decoration:
-                    CustomInputDecoration.commonInputDecoration.copyWith(
-                  errorText: (_birthDateVisited || _formSubmitted) &&
-                          selectedBirthDate == null
-                      ? 'Por favor selecciona una fecha'
-                      : null,
-                  errorMaxLines: 2,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      selectedBirthDate == null
-                          ? 'Seleccionar fecha'
-                          : DateFormat.yMd(currentLocale)
-                              .format(selectedBirthDate!),
-                      style: selectedBirthDate == null
-                          ? CustomInputDecoration
-                              .commonInputDecoration.hintStyle
-                          : Theme.of(context)
-                              .textTheme
-                              .titleMedium
-                              ?.copyWith(color: Color(0xFF1A1A1A)),
-                    ),
-                    const Icon(Icons.calendar_today, color: Color(0xFF8F9098)),
-                  ],
-                )),
-          ),
+          _buildBirthDateField(),
           const SizedBox(height: 24),
-          Text('Nivel de estudios', style: _labelStyle),
-          const SizedBox(height: 8),
-          DropdownButtonFormField<EducationLevel>(
-            value: selectedEducationLevel,
-            decoration: CustomInputDecoration.commonInputDecoration.copyWith(),
-            hint: Text(
-              'Seleccionar nivel de estudios',
-              style: CustomInputDecoration.commonInputDecoration.hintStyle,
-            ),
-            style: Theme.of(context)
-                .textTheme
-                .titleMedium
-                ?.copyWith(color: Color(0xFF1A1A1A)),
-            items: EducationLevel.values
-                .map((level) => DropdownMenuItem(
-                      value: level,
-                      child: Text(getEducationLevelText(level)),
-                    ))
-                .toList(),
-            onChanged: (EducationLevel? value) {
-              setState(() {
-                selectedEducationLevel = value;
-                _educationLevelVisited = true;
-                _nicknameVisited = true;
-                _sexVisited = true;
-                _birthDateVisited = true;
-              });
-              _formKey.currentState?.validate();
-            },
-            validator: (value) {
-              if ((_educationLevelVisited || _formSubmitted) && value == null) {
-                return 'Por favor selecciona un nivel de estudios';
-              }
-              return null;
-            },
-            isExpanded: true,
-          ),
+          _buildEducationLevelField(),
         ],
       ),
+    );
+  }
+
+  Widget _buildNicknameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Apodo', style: _labelStyle),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: nicknameController,
+          focusNode: nicknameFocusNode,
+          decoration: CustomInputDecoration.commonInputDecoration().copyWith(
+            hintText: 'Introduce un apodo',
+          ),
+          validator: (value) {
+            if ((_nicknameVisited || _formSubmitted)) {
+              if (nicknameFocusNode.hasFocus) {
+                return null;
+              }
+              if (value == null || value.isEmpty) {
+                return 'Por favor introduce un apodo';
+              }
+            }
+            return null;
+          },
+          onTap: () => setState(() => _nicknameVisited = true),
+          onChanged: (value) {
+            setState(() => _nicknameVisited = true);
+            _formKey.currentState?.validate();
+          },
+          style: CustomInputDecoration.textInputStyle,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSexField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Sexo', style: _labelStyle),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 150,
+              child: RadioListTile<Sex>(
+                title: const Text('Masculino'),
+                value: Sex.male,
+                groupValue: selectedSex,
+                activeColor: CustomInputDecoration.focusColor,
+                onChanged: (Sex? value) {
+                  setState(() {
+                    selectedSex = value;
+                    _sexVisited = true;
+                    _nicknameVisited = true;
+                  });
+                  _formKey.currentState?.validate();
+                },
+                contentPadding: EdgeInsets.zero,
+                visualDensity: VisualDensity.compact,
+              ),
+            ),
+            SizedBox(width: 20),
+            SizedBox(
+              width: 150,
+              child: RadioListTile<Sex>(
+                title: const Text('Femenino'),
+                value: Sex.female,
+                groupValue: selectedSex,
+                activeColor: CustomInputDecoration.focusColor,
+                onChanged: (Sex? value) {
+                  setState(() {
+                    selectedSex = value;
+                    _sexVisited = true;
+                    _nicknameVisited = true;
+                  });
+                  _formKey.currentState?.validate();
+                },
+                contentPadding: EdgeInsets.zero,
+                visualDensity: VisualDensity.compact,
+              ),
+            ),
+          ],
+        ),
+        if ((_sexVisited || _formSubmitted) && selectedSex == null)
+          Padding(
+            padding: const EdgeInsets.only(top: 0, left: 12.0, bottom: 8.0),
+            child: Text(
+              'Por favor selecciona un sexo',
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.error, fontSize: 12),
+            ),
+          ),
+      ],
+    );
+  }
+
+  Widget _buildBirthDateField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Fecha de nacimiento', style: _labelStyle),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: birthDateController,
+          focusNode: birthDateFocusNode,
+          readOnly: true,
+          decoration: CustomInputDecoration.commonInputDecoration().copyWith(
+            hintText: 'Seleccionar fecha',
+            suffixIcon: Icon(Icons.calendar_today, color: Color(0xFF8F9098)),
+          ),
+          onTap: () => _selectDate(context),
+          validator: (value) {
+            if ((_birthDateVisited || _formSubmitted)) {
+              if (birthDateFocusNode.hasFocus) {
+                return null;
+              }
+              if (selectedBirthDate == null) {
+                return 'Por favor selecciona una fecha';
+              }
+            }
+            return null;
+          },
+          style: CustomInputDecoration.textInputStyle,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEducationLevelField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Nivel de estudios', style: _labelStyle),
+        const SizedBox(height: 8),
+        DropdownButtonFormField<EducationLevel>(
+          value: selectedEducationLevel,
+          focusNode: educationLevelFocusNode,
+          decoration: CustomInputDecoration.commonInputDecoration().copyWith(),
+          hint: Text(
+            'Seleccionar nivel de estudios',
+            style: CustomInputDecoration.commonInputDecoration().hintStyle,
+          ),
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium
+              ?.copyWith(color: Color(0xFF1A1A1A)),
+          items: EducationLevel.values
+              .map((level) => DropdownMenuItem(
+                    value: level,
+                    child: Text(getEducationLevelText(level)),
+                  ))
+              .toList(),
+          onChanged: (EducationLevel? value) {
+            setState(() {
+              selectedEducationLevel = value;
+              _educationLevelVisited = true;
+              _nicknameVisited = true;
+              _sexVisited = true;
+              _birthDateVisited = true;
+            });
+            _formKey.currentState?.validate();
+          },
+          validator: (value) {
+            if ((_educationLevelVisited || _formSubmitted)) {
+              if (educationLevelFocusNode.hasFocus) {
+                return null;
+              }
+              if (value == null) {
+                return 'Por favor selecciona un nivel de estudios';
+              }
+            }
+            return null;
+          },
+          isExpanded: true,
+        ),
+      ],
     );
   }
 
