8a08c517b6c545186f3f69b9c5f15a9165e2ef75
xiaoyang <xiao1.developer@gmail.com>
2025-04-14 19:48:10 +0200

    feat : Remote reference code validation with message field

--- lib/app/features/home/domain/usecases/validate_reference_code_use_case.dart
+++ lib/app/features/home/domain/usecases/validate_reference_code_use_case.dart
@@ -1,3 +1,5 @@
+import '../../../../utils/helpers/app_helpers.dart';
+import '../../../../utils/services/net/rest_api_services.dart';
 import '../entities/reference_validation_result_entity.dart';
 import '../repository/reference_validation_repository.dart';
 
@@ -22,8 +24,18 @@ class ValidateReferenceCodeUseCase {
     final resultData =
         await repository.validateReferenceCodeRemotely(referenceCode);
 
-    if (resultData.result) { //TODO valid with message
-      return ReferenceValidationResult(isValid: true, isUsedLocally: false);
+    final messageValue = resultData.data[ApiConstants.messageField];
+
+    if (resultData.result) {
+      if (StringHelper.equalsIgnoreCase(
+          messageValue, ApiConstants.referenceCodeValid)) {
+        return ReferenceValidationResult(isValid: true, isUsedLocally: false);
+      } else {
+        return ReferenceValidationResult(
+          isValid: false,
+          isUsedLocally: false
+        );
+      }
     } else {
       return ReferenceValidationResult(
         isValid: false,
