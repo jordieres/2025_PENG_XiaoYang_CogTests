a86772d9dd6c2128a4c1ee81786fb8638cedb156
xiaoyang <xiao1.developer@gmail.com>
2025-04-15 21:29:22 +0200

    Refactor: Update AppSnackbar to use GetX for improved snackbar management
    The AppSnackbar class has been updated to utilize GetX for displaying snackbars. This change replaces the previous implementation that used ScaffoldMessenger. The new approach leverages GetX's snackbar functionality, providing a more concise and potentially more efficient way to manage snackbars within the application.

--- lib/app/utils/ui/app_snackbar.dart
+++ lib/app/utils/ui/app_snackbar.dart
@@ -1,15 +1,14 @@
 part of ui_utils;
 
-/// contains all snackbar templates
 class AppSnackbar {
   static bool _isSnackbarVisible = false;
 
   static void showCustomSnackbar(
-    BuildContext context,
-    String message, {
-    int durationInSeconds = 2,
-    Color? backgroundColor,
-  }) {
+      BuildContext context,
+      String message, {
+        int durationInSeconds = 2,
+        Color? backgroundColor,
+      }) {
     if (_isSnackbarVisible) {
       return;
     }
@@ -17,32 +16,31 @@ class AppSnackbar {
     backgroundColor ??= Theme.of(context).colorScheme.inverseSurface;
 
     _isSnackbarVisible = true;
-    final screenWidth = MediaQuery.of(context).size.width;
 
-    ScaffoldMessenger.of(context)
-        .showSnackBar(
-          SnackBar(
-            content: Center(
-              child: Text(
-                message,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Theme.of(context).colorScheme.onInverseSurface),
-              ),
-            ),
-            duration: Duration(seconds: durationInSeconds),
-            behavior: SnackBarBehavior.floating,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            margin: EdgeInsets.symmetric(
-              horizontal: screenWidth * 0.1, // 20% padding on both sides
-              vertical: 20,
-            ),
-            backgroundColor: backgroundColor,
+    Get.snackbar(
+      '',
+      '',
+      titleText: Container(),
+      messageText: Center(
+        child: Text(
+          message,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onInverseSurface,
           ),
-        )
-        .closed
-        .then((_) {
+        ),
+      ),
+      duration: Duration(seconds: durationInSeconds),
+      snackPosition: SnackPosition.BOTTOM,
+      backgroundColor: backgroundColor,
+      margin: EdgeInsets.symmetric(
+        horizontal: Get.width * 0.1,
+        vertical: 20,
+      ),
+      borderRadius: 10,
+      isDismissible: true,
+      forwardAnimationCurve: Curves.easeOutCirc,
+    ).future.then((_) {
       _isSnackbarVisible = false;
     });
   }
